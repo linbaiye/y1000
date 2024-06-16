68810e235c84f9d8cd245da76144bc4d4a75e8bc
Kyle <linbaiye@gmail.com>
2024-06-17 00:24:49 +0800

    Add files.

--- Source/Control/Bottom/UsedKungFuLabel.cs
+++ Source/Control/Bottom/UsedKungFuLabel.cs
@@ -21,8 +21,9 @@ public partial class UsedKungFuLabel : RichTextLabel
         if (_kungFuName.Equals(text))
         {
             ProcessMode = ProcessModeEnum.Inherit;
-            _blinkingTime = 1.5;
-            Text = "[rainbow freq=2.0 sat=0.8 val=1]" + CenterText() + "[/rainbow]";
+            _blinkingTime = 1;
+            Text = "[pulse freq=7 color=#6193df ease=-2.0]" + CenterText() + "[/pulse]";
+            //Text = "[rainbow freq=2.0 sat=0.8 val=1]" + CenterText() + "[/rainbow]";
             return true;
         }
         return false;
