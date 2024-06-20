84d2400a74bb5be633305bba1b7b4cdff9ec19a8
Kyle Lin <kyle.lin@wirelesscar.com>
2024-06-20 18:00:30 +0800

    back

--- Source/Sprite/FilesystemSpriteRepository.cs
+++ Source/Sprite/FilesystemSpriteRepository.cs
@@ -12,7 +12,7 @@ public class FilesystemSpriteRepository: AbstractSpriteRepository
 
     private static readonly ILogger LOG = LogManager.GetCurrentClassLogger();
     private static readonly string DIR_PATH = "../sprite/";
- //   private static readonly string DIR_PATH = "D:/work/sprite/";
+  //  private static readonly string DIR_PATH = "D:/work/sprite/";
     private const bool CacheEnabled = true;
     private static readonly IDictionary<string, AtzSprite> Cache = new Dictionary<string, AtzSprite>();
     private FilesystemSpriteRepository()
