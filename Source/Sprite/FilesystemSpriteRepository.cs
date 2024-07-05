dcc39e3e1cbe0085723ae70681aa36f6c29ecec0
Kyle <linbaiye@gmail.com>
2024-07-05 09:33:51 +0800

    ‘add’

--- Source/Sprite/FilesystemSpriteRepository.cs
+++ Source/Sprite/FilesystemSpriteRepository.cs
@@ -12,8 +12,8 @@ public class FilesystemSpriteRepository: AbstractSpriteRepository
     public static readonly FilesystemSpriteRepository Instance = new();
 
     private static readonly ILogger LOG = LogManager.GetCurrentClassLogger();
-    private static readonly string DIR_PATH = "../sprite/";
-    //private static readonly string DIR_PATH = "D:/work/sprite/";
+    //private static readonly string DIR_PATH = "../sprite/";
+    private static readonly string DIR_PATH = "D:/work/sprite/";
     private const bool CacheEnabled = true;
     private static readonly IDictionary<string, AtzSprite> Cache = new Dictionary<string, AtzSprite>();
     private readonly MonsterSdbReader _monsterSdb = MonsterSdbReader.Instance;
